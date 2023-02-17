556494a0bf9e33710a4a0c5de33c17d7c61e8e99
wang.yubin <359474377@qq.com>
2023-02-17 15:30:02 +0800

    增加项目初次访问index时，无法跳转到正确的index页面的说明

--- source-web-module/src/main/java/com/xiacybing/servlet/IndexServlet.java
+++ source-web-module/src/main/java/com/xiacybing/servlet/IndexServlet.java
@@ -20,6 +20,14 @@ public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println("正在访问：" + req.getRequestURI());
+
+        // todo 如果项目启动后，第一次就直接访问/source-web-module/index，会被跳转到Tomcat的首页，只有在访问过/source-web-module/index
+        //  .jsp后，再访问/source-web-module/index，才能正确的跳转到/source-web-module/index.jsp
+        //  从catalina-home/work/Catalina目录下看，访问不到/source-web-module/index
+        //  .jsp时，目录下的source-web-module文件夹为空，只有ROOT文件夹下有一个index_jsp类，和相应的index_jsp
+        //  .java文件，在直接访问/source-web-module/index.jsp后，source-web-module文件夹下才出现相关的index_jsp类和index_jsp
+        //  .java文件，怀疑和当初直接在org.apache.catalina.startup.ContextConfig.configureStart中增加context
+        //  .addServletContainerInitializer(new JasperInitializer(), null)有关
         resp.sendRedirect("redirect:index.jsp");
     }
 
